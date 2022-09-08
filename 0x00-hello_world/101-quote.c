5a6e9e99c552fb19e67acb655dbc33e8529789b0
Chima-Peter <chimaojimma@gmail.com>
2022-09-08 13:15:11 -0700

    im done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - Prints a statement if code runs successfully.
  *
- * Return: Code returns 0 if successful, else returns nothing.
+ * Return: Code returns 1 if successful, else returns nothing.
  */
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	return (0);
+	return (1);
 }
