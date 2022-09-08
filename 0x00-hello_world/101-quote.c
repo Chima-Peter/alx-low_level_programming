fddb7c040f36138d0678295b71992a16e8bd893d
Chima-Peter <chimaojimma@gmail.com>
2022-09-08 13:04:23 -0700

    at last

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a statement if code runs successfully.
+ *
+ * Return: Code returns 0 if successful, else returns nothing.
+ */
+int main(void)
+{
+	printf("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
