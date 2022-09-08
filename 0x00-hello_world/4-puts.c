0a7326957a9e2d58dfb796f5ad26bd97bf73a94a
Chima-Peter <chimaojimma@gmail.com>
2022-09-08 10:32:57 -0700

    guy

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase "Programming`is like building a multilingual puzzle
+ *
+ * Return: 0 if the code runs else nothing
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints \"with proper grammar, but the outcome is a piece of art\"
+ *
+ * Return: Program returns 0 if it runs correctly, else nothing
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
