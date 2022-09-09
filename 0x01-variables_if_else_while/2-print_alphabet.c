c0af309eab5083c28d40d958b4341db089bb9445
Chima-Peter <chimaojimma@gmail.com>
2022-09-09 08:44:02 -0700

    abeg o

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Code returns 0 upon successful execution.
+ */
+int main(void)
+{
+	char ch = 'A';
+
+	putchar(tolower(ch));
+
+	return (0);
+}
