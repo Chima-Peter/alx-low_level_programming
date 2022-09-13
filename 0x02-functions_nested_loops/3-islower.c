7d3928eb06ff9e1e916ef4780b6f3e925cc78b75
Chima-Peter <chimaojimma@gmail.com>
2022-09-13 16:09:24 -0700

    fourth script

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Write a function that checks for lowercase character.
+ *
+ * islower - Checks if an interger is uppercase or lowercase.
+ *
+ * c - This is the interger to be tested.
+ *
+ * Return: Code returns 1 if lowercase and 0 if uppercase.
+ */
+int _islower(int c)
+{
+	islower(c);
+}
