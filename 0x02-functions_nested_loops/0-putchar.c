594891387a8b01083810a5d4d750120803637a4c
Chima-Peter <chimaojimma@gmail.com>
2022-09-13 10:09:56 -0700

    please work

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+/**
+ * main - Write a program that prints _putchar, followed by a new line.
+ *
+ * Return: Code returns 0 if successful.
+ */
+int main(void)
+{
+	
+	printf("_putchar\n");
+	
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+script 1 Write a program that prints _putchar, followed by a new line.
