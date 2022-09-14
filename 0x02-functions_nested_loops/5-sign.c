d3bc2fe3aa66cc94549683be1724c0da291e7f9d
Chima-Peter <chimaojimma@gmail.com>
2022-09-13 17:06:48 -0700

    hahah

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,14 +12,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return (n > 0);
+		return (1);
+
 	} else if (n < 0)
 	{
 		_putchar('-');
-		return (n < 0);
-	} else
+		return (-1);
+
+	} else if (n == 0)
 	{
 		_putchar('0');
-		return (n == 0);
+		return (0);
 	}
 }
