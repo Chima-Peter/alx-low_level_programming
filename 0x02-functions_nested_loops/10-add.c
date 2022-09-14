f33c4511eaaf45b52c5baf6c2f4eb7be62dfb689
Chima-Peter <chimaojimma@gmail.com>
2022-09-13 18:54:46 -0700

    done with mandatory

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Write a function that adds two integers and returns the result.
+ *
+ * @n:Interger 1
+ *
+ * @i: Interger 2
+ *
+ * Return: Value of added n and i.
+ */
+int add(int n, int i)
+{
+	int ch = n + i;
+
+	printf("%d\n", ch);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Write a function that prints all natural numbers from
+ * n to 98, followed by a new line.
+ *
+ * @n: Random interger.
+ *
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	for (i = n; i <= 98; i++)
+	{
+		if (i != 98)
+		{
+			printf("%d, ", n);
+		} else if (i == 98)
+		{
+			printf("%d\n", n);
+		}
+	}
+	for (i = n; i >= 98; i--)
+	{
+		if (i != 98)
+		{
+			printf("%d, ", n);
+		} else if (i == 98)
+		{
+			printf("%d\n", n);
+		}
+		return (0);
+	}
