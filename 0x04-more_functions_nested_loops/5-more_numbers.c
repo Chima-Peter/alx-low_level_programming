2c471b8c56d1ac6abc1301874eeb7cbab45852f2
Chima-Peter <chimaojimma@gmail.com>
2022-09-15 10:37:43 -0700

    nive

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,11 @@ void more_numbers(void)
 	{
 		for (c = 0; c <= 14; c++)
 		{
-			_putchar(c + '0');
+			if (c >= 10)
+			{
+			_putchar(a /  10 + '0');
+			_putchar(a % 10 + '0');
+			}
 		}
 		_putchar('\n');
 	}
