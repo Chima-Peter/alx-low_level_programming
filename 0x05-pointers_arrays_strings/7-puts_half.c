b3900244a2de02183dc10a4f5c73d9ef0ecc8a2b
Chima-Peter <chimaojimma@gmail.com>
2022-09-19 18:31:30 -0700

    whew

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -24,7 +24,7 @@ void puts_half(char *str)
 		}
 	} else if (length % 2 != 0)
 	{
-		for (n = (length - 1) / 2; n < length -1; n++)
+		for (n = (length - 1) / 2; n < length - 1; n++)
 		{
 			_putchar(str[n + 1]);
 		}
