26853fde5265255ec577ee59aa739bf9a79694ee
Chima-Peter <chimaojimma@gmail.com>
2022-09-27 07:32:55 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a string
+ *
+ * @s: String
+ *
+ * @c: Character
+ *
+ * Return: First occurence of charcter or null
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	while (1)
+	{
+		i = *s++;
+
+		if (i == c)
+		{
+			return (s - 1);
+		}
+		if (i == 0)
+		{
+			return (NULL);
+		}
+	}
+}
