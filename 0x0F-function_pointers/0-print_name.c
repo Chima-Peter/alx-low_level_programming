acaa763469bab04f3ba0d255c65bfc3c96ad9c1b
Chima-Peter <chimaojimma@gmail.com>
2022-10-12 17:17:35 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Main function
+ *
+ * @f: Pointer to function
+ *
+ * @name: Name to print
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != 0 && f != 0)
+		mma
+	{
+		f(name);
+	}
+}
