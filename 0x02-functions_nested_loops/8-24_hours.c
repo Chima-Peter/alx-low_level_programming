47e7914fbfac5c9f3a9c8d5a55dac55c504c76c7
Chima-Peter <chimaojimma@gmail.com>
2022-10-28 07:35:27 -0700

    correction

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,34 +1,46 @@
 #include "main.h"
 
 /**
- * jack_bauer - Write a function that prints every minute of the day of
- * Jack Bauer, starting from 00:00 to 23:59.
- *
- *  Return: Always 0.
+ * jack_bauer - prints every minute of the day
+ * Description: prints every minute of the day
+ * Return: void
  */
 void jack_bauer(void)
 {
-	int i, n, c, d;
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
-	for (i = '0'; i < '3'; i++)
+	hours_max = 58;
+	hours_tens = '0';
+	while (hours_tens < '3')
 	{
-		for (n = '0'; n < '4'; n++)
+		if (hours_tens == '2')
 		{
-			for (c = '0'; c < '6'; c++)
-			{
-				for (d = '0'; d <= '9'; d++)
-				{
-					if (n != '4' && n != '5' && n != '6' && n != '7' && n != '8' && n != '9')
-					{
-						_putchar(i);
-						_putchar(n);
-						_putchar(':');
-						_putchar(c);
-						_putchar(d);
-						_putchar('\n');
-					}
-				}
-			}
+			hours_max = '4';
 		}
+		hours_ones = '0';
+		while (hours_ones < hours_max)
+		{
+			minutes_tens = '0';
+			while (minutes_tens < '6')
+			{
+				minutes_ones = '0';
+				while (minutes_ones < 58)
+				{
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					minutes_ones++;
+				}
+				minutes_ones = '0';
+				minutes_tens++;
+			}
+			minutes_tens = '0';
+			hours_ones++;
+		}
+		hours_ones = '0';
+		hours_tens++;
 	}
 }
