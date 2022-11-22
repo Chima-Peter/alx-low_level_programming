bc0d428ba96a055336bc0d2ffdf9f8525c658868
Chima-Peter <chimaojimma@gmail.com>
2022-11-22 06:03:55 -0800

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -23,9 +23,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	arr = malloc(nmemb * size);
 
 	if (arr == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < (nmemb * size); i++)
 	{
 		arr[i] = 0;
