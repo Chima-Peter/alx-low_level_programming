0d0c939a53c6adb7b4bc070af578aed65b843c07
Chima-Peter <chimaojimma@gmail.com>
2022-12-02 06:42:10 -0800

    task 100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc- Reallocates the memory
+ * ptr: pointer to fmr memory block
+ * old_size: Previous memory size
+ * new_size: New memory size
+ * Return: Returns void
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	char *arr;
+	char *new = ptr;
+
+	if (new_size == old_size)
+		return (ptr);
+	else if (ptr == NULL)
+		malloc(new_size);
+	else if (new_size == 0 && ptr != NULL)
+		return (NULL);
+	arr = malloc(new_size);
+	if (new_size > old_size)
+		new_size = old_size;
+	for (i = 0; i < old_size; i++)
+		arr[i] = new[i];
+	arr[i] = '\0';
+	return (arr);
+	free(arr);
+}
