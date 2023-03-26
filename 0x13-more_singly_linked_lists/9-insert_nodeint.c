cc3c6b0e971589410b2b35cbcafee3d364c01312
Chima-Peter <chimaojimma@gmail.com>
2023-03-25 18:50:06 -0700

    corrections to 10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,8 +27,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new->n = n;
 
-	if (*head == NULL)
+	if (*head == NULL && idx != 0)
 		return (NULL);
+
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		(*head) = new;
+		return (*head);
+	}
 
 	for (i = 0; i < idx - 1; i++)
 	{
@@ -37,5 +45,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->next = (*head)->next;
 	(*head)->next = new;
 
-	return (*head);
+	return (new);
 }
