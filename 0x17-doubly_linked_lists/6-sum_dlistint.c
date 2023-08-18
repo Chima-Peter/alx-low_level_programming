36929a97884800934af83e8c69a8f89f947e968d
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-08-17 18:58:25 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,17 +1,16 @@
 #include "lists.h"
 /**
- * get_dnodeint_at_index - gets the nth node of a dlistint list
+ * sum_dlistint - sums up the n elements of a dlistint list
  *
  * @head: the dlistint list
  *
- * @index: the nth index
  *
  * Return: the nth node
  */
 int sum_dlistint(dlistint_t *head)
 {
 	unsigned int i;
-	
+
 	i = 0;
 
 	while (head != NULL)
