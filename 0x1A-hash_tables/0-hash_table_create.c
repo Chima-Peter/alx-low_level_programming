6a58c6c4bbaa81ba79145b0f5af3c7f5ac29f876
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-09 15:22:52 -0700

    ALX_LOW_LEVEL_PROGRAMMING

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - A function that creates a hash table.
+ *
+ * @size: The size of the array.
+ *
+ * Return: a pointer to the newly created hash table or NULL
+ *
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_table_t) * table->size);
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+	return (table);
+}
