f3f4b4e7b3c1f812415ab127e172109cf099886c
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-09 15:30:27 -0700

    ALX_LOW_LEVEL_PROGRAMMING

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 
 	table->size = size;
-	table->array = malloc(sizeof(hash_table_t) * table->size);
+	table->array = malloc(sizeof(hash_node_t) * table->size);
 
 	if (table->array == NULL)
 		return (NULL);
