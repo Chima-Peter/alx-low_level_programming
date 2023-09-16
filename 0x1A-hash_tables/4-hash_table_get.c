8cd313ca22728a4eb4b3e0fd423a653b18dae72f
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-16 03:32:52 -0700

    ALX_LOW_LEVEL_PROGRAMMING

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrives the value associated with a key
+ *
+ * @ht: The hash table
+ *
+ * @key: the key to retrive value for
+ *
+ * Return: either the value or null
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+
+	if (ht == NULL)
+		return (NULL);
+
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i] != NULL)
+		{
+			if (strcmp(ht->array[i]->key, key) == 0)
+				return (ht->array[i]->value);
+			ht->array[i] = ht->array[i]->next;
+		}
+	}
+	return (NULL);
+}
