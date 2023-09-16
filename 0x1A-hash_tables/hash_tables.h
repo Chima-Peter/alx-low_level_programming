77bb028625e5ee9ebca1ff11d892831913f10699
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-16 04:39:13 -0700

    ALX_LOW_LEVEL_PROGRAMMING

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -42,5 +42,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 hash_node_t *create_item(const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
-
+void hash_table_delete(hash_table_t *ht);
 #endif
